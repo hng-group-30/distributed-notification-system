5c1ec2163c95c9ac88869a68ec0ce6d4fd17b52f
root <root@DESKTOP-IG6VSSM.localdomain>
2025-11-11 11:00:36 +0100

    feat: update of email service to include delivery confirmations and bounces

--- email_service/src/app.module.ts
+++ email_service/src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { EmailService } from './email/email.service';
 import { EmailConsumer } from './email/email.consumer';
 import { HealthController } from './email/health/health.controller';
+import { MetricsController } from './email/metrics/metrics.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [HealthController, EmailConsumer],
+  controllers: [HealthController, EmailConsumer, MetricsController],
   providers: [EmailService],
 })
 export class AppModule {}

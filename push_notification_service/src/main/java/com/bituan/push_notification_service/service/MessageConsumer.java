ed41ecd3b9265ae6eaa406bb90d65020c6352b0d
bituan <113036994+bituann@users.noreply.github.com>
2025-11-11 17:39:19 +0100

    refactor: change message type from string to message model

--- push_notification_service/src/main/java/com/bituan/push_notification_service/service/MessageConsumer.java
+++ push_notification_service/src/main/java/com/bituan/push_notification_service/service/MessageConsumer.java
@@ -1,12 +1,13 @@
 package com.bituan.push_notification_service.service;
 
+import com.bituan.push_notification_service.dto.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageConsumer {
     @RabbitListener(queues = {"${rabbitmq.queue.name}"})
-    public void consume (String message) {
+    public void consume (Message message) {
         System.out.printf("Message received -> %s%n", message);
     }
 }

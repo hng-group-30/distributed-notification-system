8044b2a66853218e7eb719768c24aff5ef1e523f
bituan <113036994+bituann@users.noreply.github.com>
2025-11-11 19:32:01 +0100

    config: add support for json message in rabbitmq

--- push_notification_service/src/main/java/com/bituan/push_notification_service/config/RabbitMQConfig.java
+++ push_notification_service/src/main/java/com/bituan/push_notification_service/config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.bituan.push_notification_service.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,5 +36,20 @@ public class RabbitMQConfig {
                 .bind(queue())
                 .to(exchange())
                 .with(this.routingKey);
+    }
+
+    // configure message converter for serializing json to dto and vice versa
+    @Bean
+    public MessageConverter messageConverter () {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // template that uses serializer
+    @Bean
+    public AmqpTemplate amqpTemplate (ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+
+        return rabbitTemplate;
     }
 }

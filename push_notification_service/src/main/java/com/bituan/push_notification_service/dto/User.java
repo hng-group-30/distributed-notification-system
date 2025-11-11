15fb201e61170887d5abbb0945b37bebc8ff0c58
bituan <113036994+bituann@users.noreply.github.com>
2025-11-11 18:07:14 +0100

    refactor: add getters to user model

--- push_notification_service/src/main/java/com/bituan/push_notification_service/dto/User.java
+++ push_notification_service/src/main/java/com/bituan/push_notification_service/dto/User.java
@@ -1,18 +1,46 @@
 package com.bituan.push_notification_service.dto;
 
-import lombok.Data;
-
-@Data
 public class User {
     private String name;
     private String email;
     private String pushToken;
     private UserPreference userPreference;
     private String password;
+
+    public User() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPushToken() {
+        return pushToken;
+    }
+
+    public UserPreference getUserPreference() {
+        return userPreference;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
 
-@Data
 class UserPreference {
     private boolean email;
     private boolean push;
+
+    public UserPreference() {}
+
+    public boolean isEmail() {
+        return email;
+    }
+
+    public boolean isPush() {
+        return push;
+    }
 }

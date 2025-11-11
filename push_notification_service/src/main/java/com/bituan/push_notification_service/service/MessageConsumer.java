a369593e341a4be62165858c705964020256dac4
bituan <113036994+bituann@users.noreply.github.com>
2025-11-11 19:17:23 +0100

    refactor: add user service to message consumer

--- push_notification_service/src/main/java/com/bituan/push_notification_service/service/MessageConsumer.java
+++ push_notification_service/src/main/java/com/bituan/push_notification_service/service/MessageConsumer.java
@@ -2,10 +2,19 @@ package com.bituan.push_notification_service.service;
 
 import com.bituan.push_notification_service.dto.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageConsumer {
+
+    private UserService userService;
+
+    @Autowired
+    MessageConsumer(UserService userService) {
+        this.userService = userService;
+    }
+
     @RabbitListener(queues = {"${rabbitmq.queue.name}"})
     public void consume (Message message) {
         System.out.printf("Message received -> %s%n", message);

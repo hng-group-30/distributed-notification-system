e58e3354196165840e1b2147659b00a1fc030e3c
bituan <113036994+bituann@users.noreply.github.com>
2025-11-12 16:43:39 +0100

    refactor: complete helper service to push notification with retry

--- push_notification_service/src/main/java/com/bituan/push_notification_service/service/FirebaseFCMService.java
+++ push_notification_service/src/main/java/com/bituan/push_notification_service/service/FirebaseFCMService.java
@@ -1,9 +1,17 @@
 package com.bituan.push_notification_service.service;
 
+import com.bituan.push_notification_service.dto.NotificationStatus;
+import com.bituan.push_notification_service.dto.NotificationStatusResponse;
 import com.google.firebase.messaging.*;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class FirebaseFCMService {
@@ -17,16 +25,40 @@ public class FirebaseFCMService {
     }
 
     @Async
-    public  void pushNotification (String token, Notification notification, String notificationId) {
+    @Retryable(
+            maxAttempts = 5, // Maximum number of retry attempts
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
+    public void pushNotification (String token, Notification notification, String notificationId) {
         Message message = Message.builder().setToken(token).setNotification(notification).build();
         try {
             firebaseMessaging.send(message);
+            NotificationStatusResponse response = new NotificationStatusResponse();
+            response.setNotificationId(notificationId);
+            response.setStatus(NotificationStatus.delivered);
+            response.setTimestamp(Instant.now());
+
+            // update notification status
+            apiGatewayService.sendNotificationStatus(response);
         } catch (FirebaseMessagingException e) {
             if (e.getMessagingErrorCode() == MessagingErrorCode.UNREGISTERED) {
                 //update user token to null
                 apiGatewayService.updateUserToken("", null);
+            } else {
+                throw new RuntimeException(notificationId, e);
             }
-            throw new RuntimeException(e);
         }
+    }
+
+    @Recover
+    public void failedRetry (RuntimeException e) {
+        NotificationStatusResponse response = new NotificationStatusResponse();
+        response.setNotificationId(e.getMessage());
+        response.setStatus(NotificationStatus.failed);
+        response.setTimestamp(Instant.now());
+        response.setError("Failed to send. Maximum retry reached");
+
+        // update notification status
+        apiGatewayService.sendNotificationStatus(response);
     }
 }

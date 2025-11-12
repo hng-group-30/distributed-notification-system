ad736304c24d10afc17d6a16453ea6eb45f2faf4
bituan <113036994+bituann@users.noreply.github.com>
2025-11-12 10:47:02 +0100

    refactor: add helper service for pushing notifications

--- push_notification_service/src/main/java/com/bituan/push_notification_service/service/FirebaseFCMService.java
+++ push_notification_service/src/main/java/com/bituan/push_notification_service/service/FirebaseFCMService.java
@@ -1,4 +1,29 @@
 package com.bituan.push_notification_service.service;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FirebaseFCMService {
+    private final FirebaseMessaging firebaseMessaging;
+
+    @Autowired
+    FirebaseFCMService (FirebaseMessaging firebaseMessaging) {
+        this.firebaseMessaging = firebaseMessaging;
+    }
+
+    @Async
+    public  void pushNotification (String token, Notification notification, String notificationId) {
+        Message message = Message.builder().setToken(token).setNotification(notification).build();
+        try {
+            firebaseMessaging.send(message);
+        } catch (FirebaseMessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
